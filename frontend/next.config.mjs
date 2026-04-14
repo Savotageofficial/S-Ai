760f857073f23636b90d9c8b6dffc8a6975b8665
palmhillstry <mohamedgamesar4321@gmail.com>
2026-04-14 20:21:46 +0200

    removed the s from the http

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://ms-ai.duckdns.org/:path*",
+        destination: "http://ms-ai.duckdns.org/:path*",
       },
     ];
   },

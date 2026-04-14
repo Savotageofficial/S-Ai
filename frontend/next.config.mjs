2a93455f64002d92b7dc678ee10e2a5888dbbdc8
palmhillstry <mohamedgamesar4321@gmail.com>
2026-04-15 00:12:59 +0200

    making the front call the backend on port 8000

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://ms-ai.duckdns.org/:path*",
+        destination: "http://ms-ai.duckdns.org:8000/:path*",
       },
     ];
   },

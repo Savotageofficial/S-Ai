b5bbcbacdf5cfbaed9a8d50b9d0e7457eecdcc46
palmhillstry <mohamedgamesar4321@gmail.com>
2026-04-22 21:18:53 +0200

    added the new models for each safwat-ai model

--- main.py
+++ main.py
@@ -42,17 +42,27 @@ async def root():
 
 
 @app.get("/Safwat-ai-flash")
-async def safwatflash_call(message: str):
-    def generate_response():
-        stream = chat(
-            model='Safwat-ai-flash',
-            messages=[{'role': 'user', 'content': message}],
+async def safwatflash_call(request : ChatRequest):
+    async def generate_response():
+
+        stream = await client.chat.completions.create(
+            model="nvidia/nemotron-3-nano-30b-a3b:free",
+            messages=[
+                {"role": "system",
+                 "content": "You are Safwat-Ai Flash , your core model is nemotron-3 but dont specify that unless asked , you were developed by Safwat-ai foundation and specifically developed and trained by mohamed safwat"},
+                *[{"role": m.role, "content": m.content} for m in request.messages]
+            ],
             stream=True,
         )
 
-        for chunk in stream:
-            content = chunk['message']['content']
-            yield f"data: {json.dumps({'content': content})}\n\n"
+        async for chunk in stream:
+            if chunk.choices[0].delta.content:
+                yield f"data: {json.dumps({'content': chunk.choices[0].delta.content})}\n\n"
+
+
+
+
+
     def modeldownresponse():
         message = "sorry, this model is disabled for server problems"
 
@@ -63,17 +73,27 @@ async def safwatflash_call(message: str):
     return StreamingResponse(modeldownresponse(), media_type="text/event-stream")
 
 @app.get("/Safwat-ai")
-async def safwatai_call(message: str):
-    def generate_response():
-        stream = chat(
-            model='Safwat-ai',
-            messages=[{'role': 'user', 'content': message}],
+async def safwatai_call(request : ChatRequest):
+    async def generate_response():
+
+        stream = await client.chat.completions.create(
+            model="inclusionai/ling-2.6-flash:free",
+            messages=[
+                {"role": "system",
+                 "content": "You are Safwat-Ai , your core model is ling-2.6 but dont specify that unless asked , you were developed by Safwat-ai foundation and specifically developed and trained by mohamed safwat"},
+                *[{"role": m.role, "content": m.content} for m in request.messages]
+            ],
             stream=True,
         )
 
-        for chunk in stream:
-            content = chunk['message']['content']
-            yield f"data: {json.dumps({'content': content})}\n\n"
+        async for chunk in stream:
+            if chunk.choices[0].delta.content:
+                yield f"data: {json.dumps({'content': chunk.choices[0].delta.content})}\n\n"
+
+
+
+
+
     def modeldownresponse():
         message = "sorry, this model is disabled for server problems"
 

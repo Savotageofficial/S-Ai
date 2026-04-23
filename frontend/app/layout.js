3dfec916b4ef0f615f4a40bb553c829f65ef5f49
palmhillstry <mohamedgamesar4321@gmail.com>
2026-04-23 22:41:07 +0200

    changed the favicon

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/logo.png" type="image/png" />
+        <link rel="icon" href="/astrict_dark.jpeg" type="image/png" />
       </head>
       <body>{children}</body>
     </html>
